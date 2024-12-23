8a9c4e6227b8613072bd6d8cda44995ad32c2fb0
Tinz1232311 <nguyentin8704@gmail.com>
2024-12-23 19:13:31 +0700

    Finish

---
+++ WebCki/src/main/java/Content/EmailContent.java
@@ -0,0 +1,8 @@
+package Content;
+
+public class EmailContent {
+	   public static final String SMTP_HOST = "smtp.gmail.com"; // SMTP server của Gmail
+	   public static final int SMTP_PORT = 587;
+	   public static final String EMAIL_USERNAME = "nguyentin8704@gmail.com"; // Email của bạn
+	   public static final String EMAIL_PASSWORD = "dqxn uzjw lxjk aqoi"; // Mật khẩu ứng dụng
+}
---
+++ WebCki/src/main/java/Content/Iconstant.java
@@ -0,0 +1,10 @@
+package Content;
+
+public class Iconstant {
+	public static final String GOOGLE_CLIENT_ID = "778099638036-o5eus12d1pp8q2mg78gbevom3m05t3l9.apps.googleusercontent.com";
+	public static final String GOOGLE_CLIENT_SECRET = "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";
+	public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/WebCki/GoogleLog";
+	public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+	public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v2/userinfo?access_token=";
+}
